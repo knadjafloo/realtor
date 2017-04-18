bd3c8877680af41426b3c435cbb46cfe916f1dd7
Kamyar Nadjafloo <knadjafloo@gmail.com>
2017-04-17 22:23:19 -0700

    initial checkin

---
+++ README.md
@@ -0,0 +1,8 @@
+# redux-app
+
+Uses Implicit flow to gain access to your friends info and your profile settings to display a summary of the fitbit activities.
+
+index.html
+webpack --config webpack.config.js
+
+the project can be viewed at https://knadjafloo.github.io/ 
---
+++ src/components/app.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+ 
+
+  componentWillMount() {
+  
+  }
+  render() {
+    return (
+      <div>
+        Rebecca Chen
+      </div>
+    );
+  }
+}
